a954194c5cd8f4c2c7f5b503b588bbada85f4438
qct <tango.qct@gmail.com>
2018-06-09 06:09:05 +0000

    modify install.sh, fix client jar name bug

--- swagger-server/bin/install.sh
+++ swagger-server/bin/install.sh
@@ -5,6 +5,7 @@ CLIENT_HOME=$pwd/../../client
 SERVER_HOME=$pwd/../../server
 ASCII_HOME=$pwd/../../asciidoc
 DOC_HOME=$pwd/../../docs
+CLIENT_JAR="swagger-codegen-cli-2.2.3.jar"
 
 echo "generated client source folder: $CLIENT_HOME"
 echo "generated server source folder: $SERVER_HOME"
@@ -13,31 +14,31 @@ echo "generated server source folder: $SERVER_HOME"
 rm -rf $CLIENT_HOME $SERVER_HOME $ASCII_HOME $DOC_HOME
 
 # clients
-java -jar swagger-codegen-cli.jar generate \
+java -jar $CLIENT_JAR generate \
   -i $pwd/../src/main/resources/swagger.yml \
   -l java \
   -c generate-client-java-config.json \
   -o $CLIENT_HOME/java
 
-java -jar swagger-codegen-cli.jar generate \
+java -jar $CLIENT_JAR generate \
   -i $pwd/../src/main/resources/swagger.yml \
   -l go \
   -o $CLIENT_HOME/go
 
-java -jar swagger-codegen-cli.jar generate \
+java -jar $CLIENT_JAR generate \
   -i $pwd/../src/main/resources/swagger.yml \
   -l html2 \
   -c generate-client-html-config.json \
   -o $CLIENT_HOME/html2
 
 # servers
-java -jar swagger-codegen-cli.jar generate \
+java -jar $CLIENT_JAR generate \
   -i $pwd/../src/main/resources/swagger.yml \
   -l spring \
   -c generate-server-spring-config.json \
   -o $SERVER_HOME/spring
 
-java -jar swagger-codegen-cli.jar generate \
+java -jar $CLIENT_JAR generate \
   -i $pwd/../src/main/resources/swagger.yml \
   -l jaxrs-resteasy \
   -c generate-server-jaxrs-resteasy-config.json \
